26e7ea57332fe3838d2ccc9d425e7ae32b48dc99
assignment1 <3303414591@qq.com>
2021-07-12 22:38:59 +0800

    django

--- bysms/wordtrans/models.py
+++ bysms/wordtrans/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import mongoengine
 #
 # Create your models here.
-from django import models
+# from django import models
 
 class C2ecol(mongoengine.Document):
     traditional = mongoengine.StringField(max_length=255)
