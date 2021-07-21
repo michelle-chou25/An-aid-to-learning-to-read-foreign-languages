95037c686b2988886b95628dd81ebbb159c4f130
assignment1 <3303414591@qq.com>
2021-07-21 15:37:08 +0800

    change the django constructure

--- bysms/bysms/settings.py
+++ bysms/bysms/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'bysms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],       # 修改位置
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,7 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+HERE = os.path.dirname(os.path.abspath(__file__))
+HERE = os.path.join(HERE, '../')
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(HERE, 'static/'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ bysms/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import C2ecol
+
+# Create your views here.
+from django.http import HttpResponse, JsonResponse
+import json
+
+# word translation function, support both traditional and simplified Chinese
+def word(request):
+    if request.method == "POST":
+        req = json.loads(request.body)
+        word = req.get("word")
+        result = C2ecol.objects.filter(traditional=word)
+        if not result:
+            result = C2ecol.objects.filter(simplified=word)
+            if not result: # translation not found
+                return JsonResponse({"status":400,"data":"","msg":"query fails."})
+            else:
+                data = {"simplified": result[0].simplified, "pinyin": result[0].pinyin,
+                        'definitions': result[0].definitions[0]}
+                return JsonResponse({"status": 200, "data": data, "msg": "query runs successfully."})
+        else:
+            data = {"simplified": result[0].simplified, "pinyin": result[0].pinyin,
+                    'definitions': result[0].definitions[0]}
+            return JsonResponse({"status": 200, "data": data, "msg": "query runs successfully."})
+
+
+def index(request):
+    return render(request, 'index.html')
