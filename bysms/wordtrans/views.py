f8867d44137bfc96fc49d287cece16bb3572bd02
xuexue19980909 <3303414591@qq.com>
2021-08-17 16:52:06 +0800

    index and views.py

--- bysms/wordtrans/views.py
+++ bysms/wordtrans/views.py
@@ -1,3 +1,4 @@
+import speech_recognition
 from django.shortcuts import render
 from .models import C2ecol
 from Transformer import compose
@@ -13,6 +14,9 @@ import urllib.request
 import ssl
 import random
 from hashlib import md5
+import calendar, time, os
+
+
 
 ssl._create_default_https_context = ssl._create_stdlib_context
 
@@ -21,12 +25,13 @@ appid = '20210625000871960'
 appkey = '0QMEkNIdfW2LudZqhD4U'
 
 # For list of language codes, please refer to `https://api.fanyi.baidu.com/doc/21`
-from_lang = 'auto' # 自动检测语种
-to_lang = 'en'
+# from_lang = 'auto'  # 自动检测语种
+# to_lang = 'en'
 
 endpoint = 'http://api.fanyi.baidu.com'
 path = '/api/trans/vip/translate'
 baiduUrl = endpoint + path
+wit_key = "OLY5OPYPMITJBN6Z7T2ERSL3MOQHFTJV"
 
 
 # Generate salt and sign
@@ -43,6 +48,13 @@ def word(request):
         # tell an url from characters
         if re.match(r'^https?:/{2}\w.+$', url):
             print("This looks like a valid url.")
+            # id = req.get("id")
+            # if id == "mainButton1":
+            #         from_lang = "zh"
+            #         to_lang = "en"
+            # else:
+            #     from_lang = "en"
+            #     to_lang="zh"
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                                      'Chrome/51.0.2704.63 Safari/537.36'}
             req = urllib.request.Request(url=url, headers=headers)
@@ -73,21 +85,22 @@ def word(request):
                 tempResult = tempResult + item['dst']
 
             data = {"content": infoStr, "definitions": tempResult}
-            return JsonResponse({"status": 200, "data": data, "msg": "word query runs successfully."})
+            return JsonResponse({"status": 200, "data": data, "msg": "url translation runs successfully."})
         else:
-            word= req.get("word")
+            word = req.get("word")
             id = req.get("id")
             print(id)
             print(word)
             for ch in word:
-            #     if u'\u4e00' <= ch <= u'\u9fff': #如果word中存在中文
-                if id == "mainButton1" and u'\u4e00' <= ch <= u'\u9fff': #如果source为Chinese且源语言有中文
+                #     if u'\u4e00' <= ch <= u'\u9fff': #如果word中存在中文
+                if id == "mainButton1" and u'\u4e00' <= ch <= u'\u9fff':  # 如果source为Chinese且源语言有中文
                     result = C2ecol.objects.filter(traditional=word)
                     if not result:
                         result = C2ecol.objects.filter(simplified=word)
-                        if not result: # translation not found
+                        if not result:  # translation not found
                             result = compose.get(word)
-                            return JsonResponse({"status":200,"data": result, "msg":"chinese sentence query runs successfully."})
+                            return JsonResponse(
+                                {"status": 200, "data": result, "msg": "chinese sentence query runs successfully."})
                         else:
                             data = {"simplified": result[0].simplified, "pinyin": result[0].pinyin,
                                     'definitions': result[0].definitions[0]}
@@ -96,18 +109,80 @@ def word(request):
                         data = {"simplified": result[0].simplified, "pinyin": result[0].pinyin,
                                 'definitions': result[0].definitions[0]}
                         return JsonResponse({"status": 200, "data": data, "msg": "word query runs successfully."})
-                if id == "mainButton2" and (u'\u0041' <= ch <= u'\u005a' or u'\u0061' <= ch <= u'\u007a'): #如果source为English且源语言有英文
+                if id == "mainButton2" and (
+                        u'\u0041' <= ch <= u'\u005a' or u'\u0061' <= ch <= u'\u007a'):  # 如果source为English且源语言有英文
                     result = C2ecol.objects.filter(definitions=word)
                     if not result:
                         result = compose.get_en(word)
-                        return JsonResponse({"status":200,"data": result, "msg":"sentence query runs successfully."})
+                        return JsonResponse({"status": 200, "data": result, "msg": "sentence query runs successfully."})
                     else:
                         data = {"simplified": result[0].simplified, "pinyin": result[0].pinyin,
-                                    'definitions': result[0].definitions[0]}
+                                'definitions': result[0].definitions[0]}
                         return JsonResponse({"status": 200, "data": data, "msg": "english word query runs successfully."})
                 else:
-                    return JsonResponse({"status":200,"data": word, "msg":"translate failed"})
-    
+                    return JsonResponse({"status": 200, "data": word, "msg": "translate failed"})
+
+# speech recognition
+def recognize(requests):
+    if requests.method == "POST":
+        try:
+            info_str=""
+            type = requests.GET.get('type',1)
+            f =requests.FILES.get("audio", None)
+            ts = calendar.timegm(time.gmtime())
+            filePath = os.path.join('./static/video/',str(ts)+'_'+f.name+'.wav')
+            with open(filePath,'wb') as fp:
+                for part in f.chunks():
+                    fp.write(part)
+            print(type)
+            if type == '1':
+                print("Chinese")
+                # import os
+                # import sys
+                #
+                # # sys.path.append('the path needed to be imported as a modual')
+                # # add the path of its uppper directory to path
+                # path = os.path.dirname(os.path.dirname(__file__))
+                # print(path)
+                # sys.path.append(path)
+                # os.path.join(os.getcwd(), "../..")
+                #
+                # from torch._C import import_ir_module
+                # from speech2text.models.conv import GatedConv
+                # from speech2text.config import pretrained_model_path
+                # # modify pretrained_model_path to relative path if don't want to have ",,"
+                # model = GatedConv.load(os.path.join('./speech2text/pretrained/', pretrained_model_path))
+                # info_str = model.predict(filePath)
+
+
+                # Using Google API
+                r = speech_recognition.Recognizer()
+                harvard = speech_recognition.AudioFile(filePath)
+                with harvard as source:
+                    r.adjust_for_ambient_noise(source, duration=0.5)
+                    audio = r.record(source)
+                text = r.recognize_google(audio_data=audio, language="cmn-Hans-CN", show_all=True)
+                info_str = text['alternative'][0]
+                print("recognized result: ", info_str)
+            else:
+               print("English")
+               r = speech_recognition.Recognizer()
+               harvard = speech_recognition.AudioFile(filePath)
+               with harvard as source:
+                   r.adjust_for_ambient_noise(source, duration=0.5)
+                   audio = r.record(source)
+                # 用witAI识别
+               info_str = r.recognize_wit(audio_data=audio, key=wit_key)
+               print("recognized result: ", info_str)
+                # 用google识别
+               # text = r.recognize_google(audio_data=audio, language="en-US", show_all=True)
+               # info_str = text['alternative'][0]
+            data = {"content": info_str, "definitions": ""}
+            return JsonResponse({"status": 200, "data": data, "msg": "speech recognized successfully."})
+        except Exception as e:
+            print(e)
+            return JsonResponse({'Recognized text': '', 'code': 600, 'message': 'error occurs！'})
+
 def index(request):
     return render(request, 'index.html')
 
