2ddaeb1a532081dd317a90ee331a94deca900949
michelle.nanjun.zhou@gmail.com <michelle.chou@live.cn>
2021-08-18 23:30:45 +0100

    add demo image

--- speech2text/examples/train.py
+++
@@ -1,8 +0,0 @@
-from . import _init_path
-from models.conv import GatedConv
-
-model = GatedConv.load("pretrained/gated-conv.pth")
-
-model.to_train()
-
-model.fit("train.manifest", "train.manifest")
